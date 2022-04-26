b4466d44edc5f2acec6c10c756eefb9b80a33fc0
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-04-26 18:58:37 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,14 +1,31 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <stdio.h>
 #include <assert.h>
 
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
-    return 200;
+    if(celcius < 50.0)
+    {
+        printf("Temperature is %.1f celcius.\n", celcius);
+        return 200;
+    }
+    else
+    {
+        printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+        return 500;
+    }
 }
 
 void alertInCelcius(float farenheit) {
@@ -20,7 +37,9 @@ void alertInCelcius(float farenheit) {
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
+        printf("alertFailureCount = %d.\n", alertFailureCount);
     }
+    
 }
 
 int main() {
@@ -28,7 +47,7 @@ int main() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
     alertInCelcius(100.0);
-    assert(alertFailureCount != 0)
+    assert(alertFailureCount != 0);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
