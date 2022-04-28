de0a87da3f37fdf8c2c49eb9ea7063de09f706da
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-04-28 13:27:30 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -11,22 +11,8 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int alertFailureCount = 0;
 
-int networkAlertStub(float celcius) {
-    
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    if(celcius < 50.0)
-    {
-        printf("Temperature is %.1f celcius.\n", celcius);
-        return 200;
-    }
-    else
-    {
-        printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-        return 500;
-    }
-}
+extern int networkAlertStub(float celcius);
+
 
 void alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
