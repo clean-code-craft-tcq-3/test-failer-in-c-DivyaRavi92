6acfa5f487be406f0cd5355898c7557b723fc74f
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-04-28 13:47:14 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -7,8 +7,8 @@ Write your code in this editor and press "Run" button to compile and execute it.
 *******************************************************************************/
 
 #include <stdio.h>
-#include "alerter.h"
 #include <assert.h>
+#include "alerter.h"
 
 extern int alertFailureCount;
 
