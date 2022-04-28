c58ffe14c08499533f7dd9ec115a5d2869170f20
DivyaRavi92 <100188062+DivyaRavi92@users.noreply.github.com>
2022-04-28 13:44:21 +0530

    Update alerterNetworkStub.c

--- alerterNetworkStub.c
+++ alerterNetworkStub.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include "alerter.h"
 
+int alertFailureCount = 0;
+
 int networkAlertStub(float celcius) {
     
     // Return 200 for ok
@@ -17,4 +19,18 @@ int networkAlertStub(float celcius) {
         printf("ALERT: Temperature is %.1f celcius.\n", celcius);
         return 500;
     }
+}
+
+void alertInCelcius(float farenheit) {
+    float celcius = (farenheit - 32) * 5 / 9;
+    int returnCode = networkAlertStub(celcius);
+    if (returnCode != 200) {
+        // non-ok response is not an error! Issues happen in life!
+        // let us keep a count of failures to report
+        // However, this code doesn't count failures!
+        // Add a test below to catch this bug. Alter the stub above, if needed.
+        alertFailureCount += 0;
+        printf("alertFailureCount = %d.\n", alertFailureCount);
+    }
+    
 }
